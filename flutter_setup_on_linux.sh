29e99d1d29d1fc08bb519723f0ea64954fc866b7
Azamino28ki <miyamizu328@outlook.jp>
2024-04-07 14:33:55 +0900

    sudo 削除

--- flutter_setup_on_linux.sh
+++ flutter_setup_on_linux.sh
@@ -2,15 +2,15 @@
 # Flutter
 cd ~/
 wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.5-stable.tar.xz -P ~/Downloads
-sudo tar -xf ~/Downloads/flutter_linux_3.19.5-stable.tar.xz -C ~/bin/
+tar -xf ~/Downloads/flutter_linux_3.19.5-stable.tar.xz -C ~/bin/
 echo '# Flutter
 export PATH=\"~jenkins/bin/flutter/bin:$PATH\"' >> ~/.bash_profile
 # android SDK
 wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip -P ~/Downloads
-sudo unzip ~/Downloads/commandlinetools-linux-11076708_latest.zip -d ~/jenkins/tmp/
-sudo mkdir -p ~/android-sdk/cmdline-tools/latest
-sudo mv /var/lib/jenkins/tmp/* /var/lib/jenkins/android-sdk/cmdline-tools/latest/
-sudo chown -R $(id -u):$(id -g) ~/android-sdk
+unzip ~/Downloads/commandlinetools-linux-11076708_latest.zip -d ~jenkins/tmp/
+mkdir -p ~/android-sdk/cmdline-tools/latest
+mv /var/lib/jenkins/tmp/* /var/lib/jenkins/android-sdk/cmdline-tools/latest/
+chown -R $(id -u):$(id -g) ~/android-sdk
 echo '# JDK\nJAVA_HOME=$(readlink -f /usr/bin/javac | sed \"s:/bin/javac::\")
 export JAVA_HOME
 PATH=$PATH:$JAVA_HOME/bin
