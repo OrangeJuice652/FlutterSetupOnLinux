b3985286926dd76c4e0fe114e8d21ce96d4554d3
Azamino28ki <miyamizu328@outlook.jp>
2024-04-07 16:03:01 +0900

    export修正

--- flutter_setup_on_linux.sh
+++ flutter_setup_on_linux.sh
@@ -5,7 +5,7 @@ wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/
 mkdir -p ~/bin
 tar -xf ~/Downloads/flutter_linux_3.19.5-stable.tar.xz -C ~/bin/
 echo '# Flutter
-export PATH=\"~jenkins/bin/flutter/bin:$PATH\"' > ~/.bash_profile
+export PATH=~jenkins/bin/flutter/bin:$PATH' > ~/.bash_profile
 # android SDK
 wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip -P ~/Downloads
 unzip ~/Downloads/commandlinetools-linux-11076708_latest.zip -d ~/tmp/
@@ -13,7 +13,7 @@ mkdir -p ~/android-sdk/cmdline-tools/latest
 mv ~/tmp/cmdline-tools/* ~/android-sdk/cmdline-tools/latest/
 chown -R $(id -u):$(id -g) ~/android-sdk
 echo '# JDK
-JAVA_HOME=$(readlink -f /usr/bin/javac | sed \"s:/bin/javac::\")
+JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 export JAVA_HOME
 PATH=$PATH:$JAVA_HOME/bin
 export PATH
